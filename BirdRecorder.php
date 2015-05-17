9dcab3cb2d2bab98288f3661203a75fa466ca434
Ben Houge <ben@benhouge.com>
2015-05-17 15:05:53 +0200

    file recording and transfer works on laptop, mobile may be a long way off; chmod thing is for real; got nudge working again, I think

--- BirdRecorder.php
+++ BirdRecorder.php
@@ -8,12 +8,15 @@ $tellme=$_FILES["birdAudio"]["name"];
 $tmp_filename=$_FILES["birdAudio"]["tmp_name"];
 // rename the temporary file (because PHP deletes the file as soon as it's done with it)
 //rename($tmp_filename, "/tmp/uploaded_audio.wav");
-rename($tmp_filename, $_FILES["birdAudio"]["name"]);
+//$new_filename="sounds/compressed/" . $_FILES["birdAudio"]["name"];
+$new_filename= 'sounds/uncompressed/' . $_FILES["birdAudio"]["name"];
+//rename($tmp_filename, $_FILES["birdAudio"]["name"]);
+rename($tmp_filename, $new_filename);
 echo $_FILES["birdAudio"]["name"];
 //chmod("/tmp/uploaded_audio.wav", 0755);
-chmod("Birds1.wav", 0755);
+chmod($new_filename, 0755);
 $messageOfDespair = "Don't even know if this would continue if I have an error";
 $logFile = fopen('despairLog.txt','w') or die("can't open file");
-fwrite($logFile, "Some news for you: " . $tellme);
+fwrite($logFile, "Some news for you: " . $new_filename);
 fclose($logFile);
 ?>
